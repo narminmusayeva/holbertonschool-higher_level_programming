703eff6ef7203a9af95419db88af6ff9b1ab446b
narminmusayeva <narminmsyva@gmail.com>
2025-01-18 19:19:14 +0000

    done

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import hidden_4
-    for i in dir(hidden_4):
-        if i[0:2] != '__':
-            print(i)
+    names_list = dir(hidden_4)
+    for name in names_list:
+        if name[0] != '_':
+            print("{}".format(name))
