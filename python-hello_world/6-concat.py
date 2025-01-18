a35b2c797d712f8baedda247de2cae1d7b275f88
narminmusayeva <narminmsyva@gmail.com>
2025-01-18 12:36:18 +0000

    done

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This is comment
-str2 = "School"
 str1 = "Holberton"
-print(f"Welcome to {str1} {str2}!")
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
