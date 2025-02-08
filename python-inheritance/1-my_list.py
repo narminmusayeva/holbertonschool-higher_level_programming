da18ea0a548cdb46243aa4d880383c1b228e03f1
narminmusayeva <narminmsyva@gmail.com>
2025-02-08 17:39:18 +0000

    done

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """
-contains in MyList class
+module for my lists (inherits form list)
 """
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class"""
-
-
+    """
+    elements of the list int type
+    return my list and sorted list
+    """
     def print_sorted(self):
-        """Print a list in sorted acsending order"""
-        print (sorted(self))
+        # Remove infinite values before sorting
+        filtered_list = [x for x in self if x != float('inf')]
+        print(sorted(filtered_list))
+
+    def append(self, item):
+        # Check if the item is infinite before appending
+        if item == float('inf'):
+            raise ValueError("cannot convert float infinity to integer")
+        super().append(item)
