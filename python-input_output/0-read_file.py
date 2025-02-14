10618009c19cfc6aa9feda2257989d84750cf578
narminmusayeva <narminmsyva@gmail.com>
2025-02-14 17:23:30 +0000

    done

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,15 +4,15 @@ This module contains a function
 """
 
 
-def read_file(filename = ""):
+def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints its content to stdout.
 
     Args:
-    filename (str): The name of the file to read. Defaults to an empty strin    g.
+    filename (str): The name of the file to read.
 
     Returns:
     None
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end = "")
+        print(f.read(), end="")
