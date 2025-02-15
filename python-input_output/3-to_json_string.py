a9586571c5d19edc3f0408d31017dae9d6266a41
narminmusayeva <narminmsyva@gmail.com>
2025-02-15 17:45:38 +0000

    done

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+"""Defines JSON representation function"""
 import json
 
 
 def to_json_string(my_obj):
     """
     Returns the JSON representation
-
-
-    Args:
-    my_obj:The object to convert to JSON str
-
-
-    Returns:
-    str:JSON representation of the object
     """
 
     return json.dumps(my_obj)
