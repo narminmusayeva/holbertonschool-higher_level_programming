a204c35f28fb60e222c5d82998d0d8ea0b8cbecb
narminmusayeva <narminmsyva@gmail.com>
2025-02-15 17:23:51 +0000

    done

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains is a function
+"""
+def append_write(filename="", text=""):
+    """
+    Append a text filne and print it
+
+    Args:
+    filename(str): The name of file to append
+
+    Returns:
+    None
+    """
+    return f.append(text)
