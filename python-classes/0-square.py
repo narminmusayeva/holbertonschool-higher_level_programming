defd351a26b0880134570ac6a23e01407893229c
narminmusayeva <narminmsyva@gmail.com>
2025-02-24 12:01:17 +0000

    done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
 class Square:
+    """
+    This is an empty class definition for Square.
+    The 'pass' statement is used as a placeholder,
+    allowing the class to be defined without any attributes or methods.
+    This can be useful for future implementation.
+    """
     pass
