8cecc66255c7490830a8c1b3d196b46166af1b81
narminmusayeva <narminmsyva@gmail.com>
2025-02-24 16:35:07 +0400

    done

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
