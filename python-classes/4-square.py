a30517b9d493c6584259b198b3357be7e1581836
narminmusayeva <narminmsyva@gmail.com>
2025-02-26 22:32:07 +0400

    done

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -29,5 +29,5 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
     
