415fba5f8a5bb2da9e41b82716b8ef103a52f86a
narminmusayeva <narminmsyva@gmail.com>
2025-04-16 22:01:16 +0400

    done

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const updateHeader = document.getElementById("update_header");
+  const header = document.querySelector("header");
+
+  updateHeader.addEventListener("click", function () {
+    header.textContent = "New Header!!!";
+  });
+});
